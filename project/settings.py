20bbcf40e707c6eb782efd3da07fdf5012519df7
unknown <oybekfayziyev97@gmail.com>
2020-09-02 22:22:47 +0300

    art blog

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'mjz4gaiw_3a)72+rihoi8mk9nlw7)@i)$ih$*r=_h#ow0=s)nd'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
+# 'djangoe-commercewebsite.herokuapp.com','127.0.0.1'
 
 # Application definition
 
@@ -67,6 +67,7 @@ AUTHENTICATION_BACKENDS = [
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -155,6 +156,13 @@ ACCOUNT_FORMS = {
 }
 
 
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
@@ -171,3 +179,5 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'oybekfayziyev97@gmail.com'
 EMAIL_HOST_PASSWORD = 'gzrqslomtfjlvarb'
 DEFAULT_FROM_EMAIL = 'Python art blog oybekfayziyev97@gmail.com'
+
+
